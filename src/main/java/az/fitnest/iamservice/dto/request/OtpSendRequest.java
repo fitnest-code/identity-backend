7cc5c9997cfa65eec735b6e7629ddc5ccd0d9a56
balash300 <balashuseynov300@gmail.com>
2026-01-25 01:43:01 +0400

    feat: implement OTP send business flow

--- src/main/java/az/fitnest/iamservice/dto/request/OtpSendRequest.java
+++ src/main/java/az/fitnest/iamservice/dto/request/OtpSendRequest.java
@@ -1,4 +1,19 @@
 package az.fitnest.iamservice.dto.request;
 
+import az.fitnest.iamservice.enums.OtpPurpose;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OtpSendRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotNull
+    private OtpPurpose purpose;
 }
--- src/main/java/az/fitnest/iamservice/dto/response/OtpSendResponse.java
+++ src/main/java/az/fitnest/iamservice/dto/response/OtpSendResponse.java
@@ -1,4 +1,13 @@
 package az.fitnest.iamservice.dto.response;
 
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OtpSendResponse {
+    private String otpSessionId;
+    private Integer expiresInSeconds;
+    private Integer resendAvailableInSeconds;
 }
