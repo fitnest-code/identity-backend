2aff20c06c092b822cd708ddfdfb7931dd61f137
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-02-05 15:09:17 +0400

    fix

--- src/main/java/az/fitnest/iam/config/OpenApiConfig.java
+++ src/main/java/az/fitnest/iam/config/OpenApiConfig.java
@@ -1,47 +1,19 @@
-package az.fitnest.iam.config;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(apiInfo())
+                .info(new Info()
+                        .title("IAM Service API")
+                        .version("1.0.0")
+                        .description("Identity and Access Management Service"))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(
-                        new io.swagger.v3.oas.models.Components()
-                                .addSecuritySchemes("bearerAuth", bearerAuthScheme())
-                );
-    }
-
-    private Info apiInfo() {
-        return new Info()
-                .title("IAM Service API")
-                .version("1.0.0")
-                .description("""
-                        Identity and Access Management (IAM) Service for Fitnest system.
-                        
-                        Responsibilities:
-                        - Authentication (Login, Refresh)
-                        - OTP (Send / Verify)
-                        - Registration
-                        - Token lifecycle (JWT + Redis)
-                        """);
-    }
-
-    private SecurityScheme bearerAuthScheme() {
-        return new SecurityScheme()
-                .name("Authorization")
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER);
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
