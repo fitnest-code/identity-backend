2b9abfdc281c621345a2937ef5ec77cc1c5b6eb3
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-02-05 20:34:05 +0400

    push

--- src/main/java/az/fitnest/iam/config/OpenApiConfig.java
+++ src/main/java/az/fitnest/iam/config/OpenApiConfig.java
@@ -5,9 +5,6 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenApiConfig {
@@ -18,13 +15,7 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("IAM Service API")
                         .version("1.0.0")
-                        .description("Identity and Access Management Service"))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
+                        .description("Identity and Access Management Service"));
+        // Security requirements removed since we're allowing all endpoints for now
     }
 }
