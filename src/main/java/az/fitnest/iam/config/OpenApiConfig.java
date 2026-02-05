244676a895906499b0ac660f5380f78e04e135ec
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-02-05 15:11:50 +0400

    fix

--- src/main/java/az/fitnest/iam/config/OpenApiConfig.java
+++ src/main/java/az/fitnest/iam/config/OpenApiConfig.java
@@ -1,3 +1,12 @@
+package az.fitnest.iam.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class OpenApiConfig {
 
