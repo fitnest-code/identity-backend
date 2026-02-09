fdf47d0a764d50c5332360c92c91751bff4a2d18
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-02-09 10:05:40 +0400

    Fix missing imports in OpenApiConfig

--- src/main/java/az/fitnest/iam/config/OpenApiConfig.java
+++ src/main/java/az/fitnest/iam/config/OpenApiConfig.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
