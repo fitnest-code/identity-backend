6b9526d0b5c09af3a2edf9da557c8e82ce35518c
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-02-11 16:39:25 +0400

    fixed namings

--- src/main/java/az/fitnest/identity/service/IdentityEventPublisher.java
+++ src/main/java/az/fitnest/identity/service/IdentityEventPublisher.java
@@ -3,9 +3,7 @@ package az.fitnest.identity.service;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Component;
 
-@Component
 @RequiredArgsConstructor
 public class IdentityEventPublisher {
     private final KafkaTemplate<String, Object> kafkaTemplate;
