9dcf374fc153aba5c1465c76e9780915aa65e6b9
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-03-02 11:11:14 +0400

    fix: update Dockerfile ENTRYPOINT for improved JVM settings and heap dump configuration

--- src/main/java/az/fitnest/identity/configuration/JacksonConfig.java
+++ src/main/java/az/fitnest/identity/configuration/JacksonConfig.java
@@ -20,6 +20,11 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 public class JacksonConfig {
 
     @Bean
+    public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+        return new Jackson2ObjectMapperBuilder();
+    }
+
+    @Bean
     @Primary
     public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
         ObjectMapper objectMapper = builder.createXmlMapper(false).build();
